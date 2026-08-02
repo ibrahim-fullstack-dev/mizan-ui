c97a957132d0059ad35c3b3676ab698737ca589b
Ibrahim Al-herby <ibrahim.fullstack.dev@gmail.com>
2026-08-02 08:29:29 +0300

    feat(purchase-payments): assemble purchase payments layout with page header and table

---
+++ src/app/features/payments/purchase-payments/purchase-payments.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PurchasePaymentsComponent } from './purchase-payments.component';
+
+describe('PurchasePaymentsComponent', () => {
+  let component: PurchasePaymentsComponent;
+  let fixture: ComponentFixture<PurchasePaymentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PurchasePaymentsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PurchasePaymentsComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
