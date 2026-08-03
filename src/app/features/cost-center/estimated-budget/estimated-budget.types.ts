8df3a538660018790ec8b3c5f966e81c5b2e115c
Ibrahim Al-herby <ibrahim.fullstack.dev@gmail.com>
2026-08-03 09:14:48 +0300

    feat(estimated-budget): define cost center estimated budget interfaces and table specifications

---
+++ src/app/features/cost-center/estimated-budget/estimated-budget.types.ts
@@ -0,0 +1,11 @@
+export interface IEstimatedBudget {
+  id: number;
+  name: string;
+  code: string;
+  periodType: string;
+  periodStart: string;
+  periodEnd: string;
+  budgetYear: string;
+  costCenter: string;
+  status: string;
+}
