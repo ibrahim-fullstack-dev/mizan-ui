921c375116eb3821279009ea090754626fc11562
ibrahim-fullstack-dev <ibrahim.fullstack.dev@gmail.com>
2026-08-12 09:15:38 +0300

    Feat/subscriptions (#104)
    * feat(subscriptions): add subscriptions feature module and components
    
    * feat(routes): register subscription module routes

---
+++ src/app/features/subscriptions/pricing-plans/pricing-plans.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { PRICING_PLANS_DATA } from './pricing-plans-constants';
+import { IPricingPlan } from './pricing-plans.types';
+import { ButtonComponent } from '@shared/components/button/button.component';
+
+@Component({
+  selector: 'app-pricing',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './pricing-plans.component.html',
+  styleUrl: './pricing-plans.component.css',
+})
+export class PricingPlansComponent {
+  plans: IPricingPlan[] = PRICING_PLANS_DATA;
+
+  onSelectPlan(plan: IPricingPlan): void {
+    console.log('Selected plan:', plan.name);
+  }
+}
---
+++ src/app/features/subscriptions/pricing-plans/pricing-plans.types.ts
@@ -0,0 +1,18 @@
+import { ButtonConfig } from '@shared/components/button/button.types';
+
+export interface IPricingPlan {
+  id: string;
+  name: string;
+  badge?: string;
+  isPopular?: boolean;
+  price: string;
+  period: string;
+  description: string;
+  buttonText: string;
+  buttonConfig: ButtonConfig;
+  features: {
+    invoices: string;
+    databaseSize: string;
+    storage: string;
+  };
+}
