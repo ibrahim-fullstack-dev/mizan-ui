264693b11b5b7588a366dc244db04096c1ea0bb3
Ibrahim Al-herby <ibrahim.fullstack.dev@gmail.com>
2026-08-17 22:37:27 +0300

    refactor(shared): refactor InputComponent to rely on InputConfig and support icons

--- src/app/shared/components/input/input.types.ts
+++ src/app/shared/components/input/input.types.ts
@@ -1,11 +1,15 @@
 // src/app/shared/components/input/input.types.ts
 
+import { LucideIconData } from 'lucide-angular';
+
 export type InputType = 'text' | 'email' | 'number' | 'password' | 'tel' | 'url' | 'date';
 
 export interface InputConfig {
   label?: string;
   type?: InputType;
   placeholder?: string;
+  icon?: LucideIconData;
+  value?: string;
   disabled?: boolean;
   required?: boolean;
   error?: string;
